8d742cacf6ce7564f44fbcc8021403f52c53c642
görkem yıldırım <76263521+gorkemyildirimm@users.noreply.github.com>
2026-04-13 22:17:00 +0200

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+            while(l<r){
+            // Skip non-alphanumeric on left
+            while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
+                l++;
+            }
+            // Skip non-alphanumeric on right
+            while (l < r && !Character.isLetterOrDigit(s.charAt(r))) {
+                r--;
+            }
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) {
+                return false;
+            }
+            l++;
+            r--;
+
+        }
+        return true;
+    }
+}
